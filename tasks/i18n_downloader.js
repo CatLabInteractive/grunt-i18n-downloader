0b7fe299ac0e8ef944f027e51d38e45509fd0953
Thijs Van der Schaeghe <thijs@catlab.be>
2015-05-25 16:50:42 +0200

    It works, but options don't work yet.

--- tasks/i18n_downloader.js
+++ tasks/i18n_downloader.js
@@ -14,11 +14,12 @@ module.exports = function(grunt) {
 
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
-	function downloadLanguage (language)
+	function downloadLanguage (language, callback)
 	{
 		grunt.log.writeln ('Fetching ' + language.url);
 		getJSON (getHttpParameters (language.token), function (data) {
 			grunt.file.write ('app/locales/' + language.token + '.json', JSON.stringify (data));
+			callback ();
 		});
 	}
 
@@ -63,8 +64,14 @@ module.exports = function(grunt) {
 
 		grunt.log.writeln ('Fetching language list');
 		getJSON (getHttpParameters (), function (data) {
+			var toProcess = data.length;
 			for (var i = 0; i < data.length; i++) {
-				downloadLanguage(data[i]);
+				downloadLanguage (data[i], function () {
+					toProcess --;
+					if (toProcess === 0) {
+						done ();
+					}
+				});
 			}
 		});
 		//done ();
